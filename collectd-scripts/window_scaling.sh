4b416523752399f944d0912165256e3d7ab33642
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-10-22 13:30:31 +0200

    fix window scaling script

--- collectd-scripts/window_scaling.sh
+++ collectd-scripts/window_scaling.sh
@@ -4,5 +4,5 @@ INTERVAL=60 #"${COLLECTD_INTERVAL:-60}"
 
 while sleep "$INTERVAL"; do
   res=$(</proc/sys/net/ipv4/tcp_window_scaling)
-  echo "PUTVAL \"${HOSTNAME}/tcp_window_scaling/gauge-max\" interval=$INTERVAL N:${res}}"
+  echo "PUTVAL \"${HOSTNAME}/tcp_window_scaling/gauge-max\" interval=$INTERVAL N:${res}"
 done
