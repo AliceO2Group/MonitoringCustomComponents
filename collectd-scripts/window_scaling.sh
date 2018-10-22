0b3784cd3cfbc6c2907cef459e9549520ee901aa
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-10-22 11:25:08 +0200

    collectd scripts

---
+++ collectd-scripts/window_scaling.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
+INTERVAL=60 #"${COLLECTD_INTERVAL:-60}"
+
+while sleep "$INTERVAL"; do
+  res=$(</proc/sys/net/ipv4/tcp_window_scaling)
+  echo "PUTVAL \"${HOSTNAME}/tcp_window_scaling/gauge-max\" interval=$INTERVAL N:${res}}"
+done
