16dfaea6ed6245a6f0c2864d884e529230184ac7
gvino <joanwine11@gmail.com>
2018-10-24 12:28:17 +0200

    First internal monitoring commit

---
+++ internal_monitoring/InternalMonitoring/Sensor.py
@@ -0,0 +1,60 @@
+from os import getcwd
+from sys import argv, exit
+from Core.SensorClass import SensorClass
+import os
+from Utilities.Utilities import readConf
+
+
+def usage():
+    print "usage: {} [-conf conf_path] "\
+        "start|stop|restart|debug]".format(argv[0])
+
+
+default_yaml_file = getcwd()+"/conf.yaml"
+allowed_command = ["start", "stop", "restart", "debug"]
+yaml_file = None
+cmd = None
+
+if __name__ == "__main__":
+    if len(argv) == 4 and argv[1] == "-conf" and argv[3] in allowed_command:
+        yaml_file = argv[2]
+        cmd = argv[3]
+    elif len(argv) == 2 and argv[1] in allowed_command:
+        yaml_file = default_yaml_file
+        cmd = argv[1]
+    else:
+        usage()
+        exit(1)
+
+    # Default paths
+    dirname = os.path.dirname(os.path.realpath(argv[0]))
+    defaultLogfile = argv[0].replace("py", "log")
+    defaultAbsLogfile = os.path.normpath(os.path.join(dirname, defaultLogfile))
+    defaultPidfile = argv[0].replace("py", "pid")
+    defaultAbsPidfile = os.path.normpath(os.path.join(dirname, defaultPidfile))
+    defaultOutfile = argv[0].replace("py", "out")
+    defaultAbsOutfile = os.path.normpath(os.path.join(dirname, defaultOutfile))
+    defaultErrfile = argv[0].replace("py", "err")
+    defaultAbsErrfile = os.path.normpath(os.path.join(dirname, defaultErrfile))
+
+    # Import YAML configuration file
+    conf = readConf(yaml_file)
+    flumeSparkSens = SensorClass(conf,
+                                 cmd=cmd,
+                                 defaultAbsOutfile=defaultAbsOutfile,
+                                 defaultAbsErrfile=defaultAbsErrfile,
+                                 defaultAbsLogfile=defaultAbsLogfile,
+                                 defaultAbsPidfile=defaultAbsPidfile)
+
+    if cmd == "debug":
+        flumeSparkSens.run()
+    else:
+        if 'start' == cmd:
+            flumeSparkSens.start()
+        elif 'stop' == cmd:
+            flumeSparkSens.stop()
+        elif 'restart' == cmd:
+            flumeSparkSens.restart()
+        else:
+            usage()
+            exit(1)
---
+++ internal_monitoring/InternalMonitoring/Utilities/Utilities.py
@@ -0,0 +1,41 @@
+
+try:
+    import logging
+    logger = logging.getLogger('Utilities')
+except Exception as e:
+    logger = None
+
+
+def print_json(json_data):
+    """
+    Print JSON in a human readible way
+    """
+
+    import json
+    try:
+        print json.dumps(json_data, indent=2)
+    except Exception as e:
+        msg = "Impossible print JSON. Exception: {}".format(e)
+        if logger is not None:
+            logger.error(msg)
+        else:
+            print(msg)
+
+
+def readConf(yaml_file):
+    """
+    Read a yaml configuration file
+    """
+
+    import yaml
+    try:
+        with open(yaml_file, 'r') as stream:
+            conf = yaml.safe_load(stream)
+            return conf
+    except Exception as e:
+        msg = "Impossible import YAML file. Exception: {}".format(e)
+        if logger is not None:
+            logger.error(msg)
+        else:
+            print(msg)
+        return None
