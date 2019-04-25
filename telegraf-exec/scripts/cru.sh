fb4c3e7755f3e52be27f5576c2ae21819dacbff1
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2019-04-25 13:50:22 +0200

    Fix sudo invokation

--- telegraf-exec/scripts/cru.sh
+++ telegraf-exec/scripts/cru.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LINES=`sudo source /etc/profile.d/modules.sh && MODULEPATH=/root/alibuild.flp/sw/MODULES/slc7_x86-64 module load ReadoutCard/latest 2>/dev/null && /root/alibuild.flp/sw/slc7_x86-64/ReadoutCard/latest/bin/roc-metrics --id 0`
+LINES=`sudo sh -c 'source /etc/profile.d/modules.sh && MODULEPATH=/root/alibuild.flp/sw/MODULES/slc7_x86-64 module load ReadoutCard/latest 2>/dev/null && /root/alibuild.flp/sw/slc7_x86-64/ReadoutCard/latest/bin/roc-metrics --id 0'`
 SHOW=0
 while read -r line; do
   if [ "${line:0:3}" = "===" ]; then
