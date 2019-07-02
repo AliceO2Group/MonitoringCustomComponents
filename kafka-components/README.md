3afd732d0bd45251f93ab3e128e0ea1b9e63568e
jvino <joanwine11@gmail.com>
2019-07-02 15:35:50 +0200

    Add Kafka custom components (#45)

---
+++ kafka-components/README.md
@@ -0,0 +1,141 @@
+
+# Kafka components
+This directory contains [Apache Kafka](https://kafka.apache.org) custom components in order to collect, process, aggregate, and cosume metrics. 
+
+Functional components:
+- [InfluxDB UDP Consumer](#influxdb-udp-consumer)
+- [Mattermost Consumer](#mattermost-consumer)
+
+### Dependencies
+- Java > 1.8
+
+### Build
+1. Clone repository
+```
+ git clone https://github.com/AliceO2Group/MonitoringCustomComponents.git && cd kafka-components 
+```
+2. Compile
+```
+ mvn clean -e install -DskipTests 
+```
+
+The generated jar (`target/o2-kafka-0.1-jar-with-dependencies.jar`) includes all components and dependencies.
+
+## Components
+
+### InfluxDB UDP Consumer
+This component retrieves messages from the Kafka cluster and forward them to an InfluxDB instance. 
+The messages need to be formatted in the [Line Protocol format](https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/).
+The component could be configured in order to send inner monitoring data to an InfluxDB instance.
+
+#### Run
+The consumer can started using the following command:
+
+```
+java -cp target/o2-kafka-0.1-jar-with-dependencies.jar \
+ ch.cern.alice.o2.kafka.connectors.InfluxdbUdpConsumer \
+ --config conf-influxdb-udp.yaml
+```
+
+#### Configuration file 
+A configuration file example is
+
+```
+general:
+   log4jfilename: ./log4j.properties
+
+kafka_consumer_config:
+   bootstrap.servers: broker1:9092,broker2:9092,broker3:9092
+   topic: input-topic
+   group.id: influxdb-udp-consumer
+   auto.offset.reset: latest
+   fetch.min.bytes: 1
+   receive.buffer.bytes: 262144
+   max.poll.records: 1000000
+
+
+sender_config:
+   hostname: <influxdb-instance-hostname>
+   port: 8089
+
+stats_config:
+   enabled: true
+   hostname: <infludb-instance-hostname>
+   port: 8090
+   period_ms: 10000
+```
+
+Tab. 1
+
+| Section | First Keyword | Mandatory | Description | Default value |
+| --------| --------------| ----------| ----------- | ------------- |
+| *general* | -          | Yes    | Defines the start of 'general' configuration section | - |
+| *general* | *log4jfilename* | Yes | Log configuration filename | - |
+| *Kafka_consumer* | - | Yes | Defines the start of 'kafka_consumer' configuration section | - |
+| *Kafka_consumer* | *bootstrap.servers* | Yes | Comma separated list of the Kafka cluster brokers | - |
+| *Kafka_consumer* | *topic* | Yes | Input topic | - |
+| *Kafka_consumer* | *group.id* | No | A unique string that identifies the consumer group this consumer belongs to used for load balancing purpose | infludb-udp-consumer |
+| *Kafka_consumer* | *fetch.min.bytes* | No | The minimum amount of data the server should return for a fetch request | 1 |
+| *Kafka_consumer* | *auto.offset.reset* | No | Policy in the case the offset in Kafka is lost: earliest/latest | latest |
+| *Kafka_consumer* | *receive.buffer.bytes* | No | The size of the TCP receive buffer to use when reading data | 262144 |
+| *Kafka_consumer* | *max.poll.records* | No | The maximum number of records returned in a single call | 1000000 |
+| *sender* | - | Yes | Defines the start of 'sender' configuration section | 
+| *sender* | *hostname* | Yes | InfluxDB instance hostname | 
+| *sender* | *port* | Yes | InfluxDB instance port | 
+| *stats* | - | Yes | Defines the start of 'stats' configuration section | 
+| *stats* | *enabled* | Yes | Set `true` to enable the self-monitoring functionality | 
+| *stats*  | *hostname* | No | Endpoint hostname | 
+| *stats*  | *port*   | No | Endpoint port |
+| *stats*  | *period_ms* | No | Statistic report period |
+
+
+
+### Mattermost Consumer
+This component retrieves messages from the Kafka cluster and forward them to the HTTP Mattermost endpoint. 
+Before it's needed to create an [Incoming Webhooks](https://docs.mattermost.com/developer/webhooks-incoming.html) 
+The retrived messages from Kafka are JSON format and compliant to the Grafana notification message format:
+
+```JSON
+{
+  "description": "Test notification - Someone is testing the alert notification within grafana",
+  "client_url" : "http://<grafana-instance>:3000",
+  "details"    : "Triggered metrics:\nHigh value: 100.000\nHigher Value: 200.000"
+}
+```
+
+The above JSON is converted in a Mattermost message with a JSON field per line. 
+Only the `description` field is mandatory, the remaining two ones are printed if present.
+
+ 
+The component could be configured in order to send inner monitoring data to an InfluxDB instance.
+
+#### Command
+The consumer is execute using the following command
+
+```
+java -cp target/o2-kafka-0.1-jar-with-dependencies.jar \
+  ch.cern.alice.o2.kafka.connectors.MattermostConsumer  \
+  --config conf-mattermost.yaml
+```
+
+#### Configuration file 
+A configuration file example is
+
+```
+general:
+   log4jfilename: ./log4j.properties
+
+kafka_consumer_config:
+   bootstrap.servers: broker1:9092,broker2:9092,broker3:9092
+   topic: notification-topic
+
+mattermostr_config:
+   url: https://<mattermost-server-hostname>/hooks/<token-id>
+
+stats_config:
+   enabled: true
+   hostname: <infludb-instance-hostname>
+   port: 8090
+   period_ms: 10000
+```
+
---
+++ kafka-components/src/main/java/ch/cern/alice/o2/kafka/connectors/InfluxdbUdpConsumer.java
@@ -0,0 +1,211 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.cern.alice.o2.kafka.connectors;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Collections;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.RetriableCommitFailedException;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+import static net.sourceforge.argparse4j.impl.Arguments.store;
+
+import java.net.SocketException;
+import java.io.File;
+
+import ch.cern.alice.o2.kafka.utils.YamlInfluxdbUdpConsumer;
+
+public class InfluxdbUdpConsumer {
+	private static Logger logger = LoggerFactory.getLogger(InfluxdbUdpConsumer.class); 
+	private static int data_endpoint_port = 0;
+	private static int stats_endpoint_port = 0;
+	private static long receivedRecords = 0;
+	private static long sentRecords = 0;
+	private static long stats_period_ms = 0;
+	private static long startMs = 0;
+	private static String stats_type;
+	private static String data_endpoint_hostname =  null;
+	private static InetAddress data_address = null;
+	private static InetAddress stats_address = null;
+	private static DatagramSocket datagramSocket;
+	
+	/* Stats parameters */
+	private static final String STATS_TYPE_INFLUXDB = "influxdb";
+	private static final String DEFAULT_STATS_TYPE = STATS_TYPE_INFLUXDB;
+	private static final String DEFAULT_STATS_PERIOD = "10000";
+	private static final String DEFAULT_STATS_ENABLED = "false";
+	
+	/* UDP sender parameters */
+	private static final String DEFAULT_HOSTNAME = "localhost";
+	private static final String DEFAULT_PORT = "8089";
+	
+	/* Kafka consumer parameters */
+	private static final String DEFAULT_AUTO_OFFSET_RESET = "latest";
+	private static final String DEFAULT_FETCH_MIN_BYTES = "1";
+	private static final String DEFAULT_RECEIVE_BUFFER_BYTES = "262144";
+	private static final String DEFAULT_MAX_POLL_RECORDS = "1000000";
+	private static final String DEFAULT_ENABLE_AUTO_COMMIT_CONFIG = "false";
+	private static final String DEFAULT_GROUP_ID_CONFIG = "influxdb-udp-consumer";
+	private static final int POLLING_PERIOD_MS = 50;
+	
+	public static void main(String[] args) throws Exception {
+        String stats_endpoint_hostname = null;
+        startMs = System.currentTimeMillis(); 
+		
+        /* Parse command line argumets */
+        ArgumentParser parser = argParser();
+        Namespace res = parser.parseArgs(args);
+        String config_filename = res.getString("config");
+        
+        /* Parse yaml configuration file */
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        YamlInfluxdbUdpConsumer config = mapper.readValue( new File(config_filename), YamlInfluxdbUdpConsumer.class);
+        String log4jfilename = config.getGeneral().get("log4jfilename");
+        PropertyConfigurator.configure(log4jfilename);
+        Map<String,String> kafka_consumer_config = config.getKafka_consumer_config();
+        Map<String,String> sender_config = config.getSender_config();
+        Map<String,String> stats_config = config.getStats_config();
+        String topicName = kafka_consumer_config.get("topic");
+        data_endpoint_hostname = sender_config.getOrDefault("hostname",DEFAULT_HOSTNAME);
+        data_endpoint_port = Integer.parseInt(sender_config.getOrDefault("port",DEFAULT_PORT));
+        boolean stats_enabled = Boolean.valueOf(stats_config.getOrDefault("enabled", DEFAULT_STATS_ENABLED));
+        stats_type = DEFAULT_STATS_TYPE;
+        stats_endpoint_hostname = stats_config.getOrDefault("hostname", DEFAULT_HOSTNAME);
+        stats_endpoint_port = Integer.parseInt(stats_config.getOrDefault("port", DEFAULT_PORT));
+        stats_period_ms = Integer.parseInt(stats_config.getOrDefault("period_ms", DEFAULT_STATS_PERIOD));
+        
+        logger.info("Data Endpoint Hostname: "+ data_endpoint_hostname);
+        logger.info("Data Endpoint Port: "+ data_endpoint_port);
+        logger.info("Stats Enabled?: "+ stats_enabled);
+        
+        /* UDP Configuration */
+        try {
+        	data_address = InetAddress.getByName(data_endpoint_hostname);
+        } catch (IOException e) {
+          logger.error("Error opening creation address using hostname: "+data_endpoint_hostname, e);
+        }
+        
+        try {
+	        datagramSocket = new DatagramSocket();
+	      } catch (SocketException e) {
+	        logger.error("Error while creating UDP socket", e);
+	    }
+        
+        if( stats_enabled ) {
+	        logger.info("Stats Type: "+ stats_type);
+	        logger.info("Stats Endpoint Hostname: "+stats_endpoint_hostname);
+	        logger.info("Stats Endpoint Port: "+stats_endpoint_port);
+	        logger.info("Stats Period: "+stats_period_ms+"ms");
+	        try {
+	        	stats_address = InetAddress.getByName(stats_endpoint_hostname);
+	        } catch (IOException e) {
+	          logger.error("Error opening creation address using hostname: "+stats_endpoint_hostname, e);
+	        }
+        }
+        
+        /* Configure Kafka consumer */
+        Properties props = new Properties();
+    	props.put(ConsumerConfig.GROUP_ID_CONFIG, kafka_consumer_config.getOrDefault(ConsumerConfig.GROUP_ID_CONFIG,DEFAULT_GROUP_ID_CONFIG));
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, DEFAULT_ENABLE_AUTO_COMMIT_CONFIG);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka_consumer_config.get(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafka_consumer_config.getOrDefault(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, DEFAULT_AUTO_OFFSET_RESET));
+        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, kafka_consumer_config.getOrDefault(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, DEFAULT_FETCH_MIN_BYTES));
+        props.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, kafka_consumer_config.getOrDefault(ConsumerConfig.RECEIVE_BUFFER_CONFIG, DEFAULT_RECEIVE_BUFFER_BYTES));
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafka_consumer_config.getOrDefault(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, DEFAULT_MAX_POLL_RECORDS));
+    	
+        KafkaConsumer<byte[],byte[]> consumer = new KafkaConsumer<byte[],byte[]>(props);
+        consumer.subscribe(Collections.singletonList(topicName));
+     
+        while (true) {
+        	try {
+                ConsumerRecords<byte[], byte[]> consumerRecords = consumer.poll(POLLING_PERIOD_MS);
+                receivedRecords += consumerRecords.count();
+                consumerRecords.forEach( record -> sendUdpData(record.value()));
+        		consumer.commitAsync();
+        		if( stats_enabled ) stats();
+            } catch (RetriableCommitFailedException e) {
+        		logger.error("Kafka Consumer Committ Exception: ",e);
+        		consumer.close();
+        		break;
+        	} catch (Exception e) {
+    			logger.error("Kafka Consumer Error: ",e);
+    			consumer.close();
+        		break;
+    		}
+        }
+	}
+	
+	private static void sendUdpData(byte[] data2send) {
+		try {
+			DatagramPacket packet = new DatagramPacket(data2send, data2send.length, data_address, data_endpoint_port);
+	        datagramSocket.send(packet);
+	        sentRecords++;
+		} catch (Exception e) {
+			logger.error("Error: ",e);
+		}
+	}
+	
+	private static void stats() throws IOException {
+		long nowMs = System.currentTimeMillis();
+    	if ( nowMs - startMs > stats_period_ms) {
+    		startMs = nowMs;
+    		String hostname = InetAddress.getLocalHost().getHostName();
+			if(stats_type.equals(STATS_TYPE_INFLUXDB)) {
+	    		String data2send = "kafka_consumer,endpoint_type=InfluxDB,endpoint="+data_endpoint_hostname+":"+data_endpoint_port+",hostname="+hostname;
+				data2send += " receivedRecords="+receivedRecords+"i,sentRecords="+sentRecords+"i "+nowMs+"000000";
+				DatagramPacket packet = new DatagramPacket(data2send.getBytes(), data2send.length(), stats_address, stats_endpoint_port);
+		        datagramSocket.send(packet);
+			}
+    	}
+	}
+    
+	private static ArgumentParser argParser() {
+        @SuppressWarnings("deprecation")
+		ArgumentParser parser = ArgumentParsers
+            .newArgumentParser("influxdb-udp-consumerr")
+            .defaultHelp(true)
+            .description("This tool is used to send UDP packets to InfluxDB.");
+
+        parser.addArgument("--config")
+    	    .action(store())
+            .required(true)
+            .type(String.class)
+            .dest("config")
+            .help("config file");
+
+        return parser;
+    }
+}	
